8a8a714e73f3fdbe7109e3ed6f241379e06c8b69
PetroHrynyk <grynyk82@gmail.com>
2022-07-20 12:37:49 +0300

    task-2

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,23 @@
-const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
-];
+// Напиши скрипт, який для кожного елемента масиву ingredients:
+
+// Створить окремий елемент < li >.Обов'язково використовуй метод
+// document.createElement().
+// Додасть назву інгредієнта як його текстовий вміст.
+// Додасть елементу клас item.
+// Після чого, вставить усі < li > за одну операцію у
+// список ul#ingredients.
+
+const ingredients = ["Potatoes", "Mushrooms", "Garlic", "Tomatos", "Herbs", "Condiments"];
+
+const ingredientsListEl = document.querySelector("#ingredients");
+// console.log(ingredientsListEl);
+
+const ingredientsListItemsEl = ingredients.map(el => {
+	const ingredientsListItemEl = document.createElement("li");
+	ingredientsListItemEl.classList.add("item");
+	ingredientsListItemEl.textContent = el;
+	return ingredientsListItemEl;
+});
+// console.log(ingredientsListItemsEl);
+
+ingredientsListEl.append(...ingredientsListItemsEl);
